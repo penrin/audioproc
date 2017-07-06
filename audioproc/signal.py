ec2b0d28e22a9645022fe0cae1fe291b6f511a9b
penrin <hrs@penr.in>
2017-07-07 07:52:56 +0900

    Add nextpow2(n), fftxcorr(x, y) and fftacorr(x) to signal.py

--- audioproc/signal.py
+++ audioproc/signal.py
@@ -14,4 +14,36 @@ def gentsp(n=18):
     upward /= np.max(np.abs(upward))
     downward = upward[::-1].copy()
     return upward, downward
-    
+
+
+# Exponent of next higher power of 2
+def nextpow2(n):
+    l = np.ceil(np.log2(n))
+    m = int(np.log2(2 ** l))
+    return m
+
+
+# Cross-correlation function
+def fftxcorr(x, y):
+    # Rxy = E[x(n) y(n + m)]
+    fftsize = 2 ** nextpow2(len(x) + len(y) - 1)
+    x_zeropad = np.zeros(fftsize)
+    y_zeropad = np.zeros(fftsize)
+    x_zeropad[:x.shape[0]] = x
+    y_zeropad[:y.shape[0]] = y
+    X = np.fft.rfft(x_zeropad)
+    Y = np.fft.rfft(y_zeropad)
+    c = np.fft.irfft(np.conj(X) * Y)
+    return c
+
+
+# Auto-correlation function
+def fftacorr(x):
+    fftsize = 2 ** nextpow2(2 * len(x) - 1)
+    x_zeropad = np.zeros(fftsize)
+    x_zeropad[:x.shape[0]] = x
+    X = np.fft.rfft(x_zeropad)
+    c = np.fft.irfft(np.conj(X) * X)
+    return c
+
+
